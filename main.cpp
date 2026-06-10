556b41d4490b7212cd6517094c0e9ef380e38d92
DavinTham <thamtorodavin@gmail.com>
2026-06-10 15:43:25 -0700

    header

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
+#ifndef SOCIAL_MEDIA_FRIEND_RECOMMENDER_H
+#define SOCIAL_MEDIA_FRIEND_RECOMMENDER_H
 
 #include <iostream>
+
+using namespace std;
 
 int main () {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+#endif 
